642e7affa9676fc260eb2f1f2d534763137b4a50
zhuoIC <nicolimonar@gmail.com>
2017-04-20 10:39:24 +0200

    Hecho en clase

---
+++ Wpf_EventosEnrutados/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_EventosEnrutados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Se lanza en cada movimiento del ratón
+            tbc1.Text = "(" + e.GetPosition(this).X + "," + e.GetPosition(this).Y + ")"+e.Source.GetType().Name;
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
